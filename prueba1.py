3f313640216c14c02f934b748018e52f405440a8
Zero <ffdezcastellanos@gmail.com>
2026-02-17 10:18:09 +0100

    Cambio

---
+++ prueba1.py
@@ -0,0 +1,11 @@
+analizar = input("Introduce que crees que es un anagrama")
+analizar = analizar.replace(" ", "")
+
+inversa = analizar[::-1]
+
+if (analizar == inversa):
+    print("El anagrama es correcto")
+else:
+    print("El anagrama no es correcto")
+print("hola")
+print("hola")
